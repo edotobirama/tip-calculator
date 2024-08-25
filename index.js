cf4aaa45e123de00053a5d28730e46fedf6f15a3
edotobirama <103825293+edotobirama@users.noreply.github.com>
2024-08-25 16:45:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function calc() {
+    let a = document.getElementById("billAmount").value;
+    let b = document.getElementById("percentageTip").value;
+    console.log(a);
+    if (a === "" || b === "") {
+        document.getElementById("errorMessage").textContent = "Please Enter a Valid Input";
+    } else {
+        a = parseInt(a);
 
+
+        b = parseInt(b);
+        let c = a * b / 100;
+        console.log(c);
+        let d = document.getElementById("tipAmount");
+        d.value = c;
+        document.getElementById("totalAmount").value = a + c;
+        document.getElementById("errorMessage").textContent = "";
+    }
+}
